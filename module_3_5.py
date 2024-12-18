9563081241ade7ab68ec8c38dc1110f60406b9ed
Nedelin222 <anedelin222@gmail.com>
2024-12-18 20:48:58 +0300

    Add files via upload

---
+++ module_3_5.py
@@ -0,0 +1,30 @@
+
+
+# def get_multiplied_digits(number):
+#     str_number = str(number)
+#     first = int(str_number[0])
+#     if first == 0:
+#         return first
+#     else:
+#         return first * get_multiplied_digits(int(str_number[1:]))
+#
+#
+# print(get_multiplied_digits(40203))
+def get_multiplied_digits(number):
+     str_number = str(number)
+     first = int(str_number[0])
+     if len(str_number) > 1:
+         return first * get_multiplied_digits(int(str_number[1:]))
+     else:
+         return first
+             
+
+# В общем разобрался, но как убрать последний "0", не могу понять что добавить надо
+
+
+
+
+print(get_multiplied_digits(40203))
+print(get_multiplied_digits(402030))
+
+#return first * get_multiplied_digits(int(str_number[1:]))
